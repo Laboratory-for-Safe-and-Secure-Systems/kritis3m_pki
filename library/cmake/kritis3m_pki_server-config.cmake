6f423b8d33bf1ad5a44aeac4e5d69ac37b57b87f
Tobias Frauenschläger <tobias.frauenschlaeger@oth-regensburg.de>
2024-08-20 13:05:40 +0200

    More CMake fixes

--- library/cmake/kritis3m_pki_server-config.cmake
+++ library/cmake/kritis3m_pki_server-config.cmake
@@ -1,3 +1,10 @@
+include(CMakeFindDependencyMacro)
+
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/kritis3m_pki_common-export.cmake)
+
+find_dependency(Threads)
+find_dependency(liboqs)
+find_dependency(wolfssl)
+
+include(${SELF_DIR}/../kritis3m_pki_common/kritis3m_pki_common-export.cmake)
 include(${SELF_DIR}/kritis3m_pki_server-export.cmake)
